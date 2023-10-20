1d42e745da00d295ddec6c518b68ad2e91dd8754
theakshaypandhare <akshaypandhare20@gmail.com>
2023-10-20 17:08:32 +0530

    Updates

---
+++ pages/invite_to_tol/invite_to_tol.jsx
@@ -0,0 +1,150 @@
+import React from 'react';
+import './invite_to_tol.css';
+import inviteimg from '../../assets/icons/invite.png'
+import cap from '../../assets/icons/111.png';
+import Statis from '../../assets/icons/Layer 3.png';
+import page from '../../assets/icons/Layer 2.png';
+import moneybag from '../../assets/icons/Layer 1.png';
+import emoji from '../../assets/icons/11.png';
+<head>
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+        crossorigin="anonymous"
+    />
+</head>
+
+
+export default function Invite() {
+    return (
+
+        <main>
+            <div className='Main'>
+                <div className='head br'>
+
+                </div>
+                <div className='white_blank' style={{ minHeight: "150px" }}>
+                    <div style={{ display: "flex", justifyContent: "center", fontWeight: "bold" }}>
+                        <h5>
+                            Invite your friends to earn rewards
+                        </h5>
+                    </div>
+                    <div style={{ display: "flex", justifyContent: "center" }}>
+                        <img src={inviteimg} alt="" />
+                    </div>
+                    <div style={{ display: "flex", justifyContent: "center", textAlign: "center", fontSize: "0.9rem", color: "#8A8CA9", fontWeight: "bold" }}>
+                        <p>If you refer friends, you’ll get multiple rewards
+                            throughout various levels.</p>
+                    </div>
+                </div>
+                <div className='grey-container' style={{ minHeight: "100px", display: 'flex', justifyContent: 'center', paddingTop: "10px" }}>
+                    <div className='innerwhitediv' style={{ minHeight: "50px", width: '94%', marginBottom: "10px" ,display: 'flex', alignContent: 'baseline',justifyContent:'center' }}>
+                        <div className='innerwhitediv br' style={{ minHeight: "50px", width: '94%'}}>
+
+                        </div>
+                        <div style={{ display: 'flex', gap: "1rem" }}>
+
+                            <img src={cap} alt="" />
+                            <img src={page} alt="" />
+                            <img src={Statis} alt="" />
+                            <img src={moneybag} alt="" />
+                            <img src={emoji} alt="" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className='grey-container' style={{ minHeight: "70px", display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <div className='innerwhitediv' style={{ minHeight: "50px", width: '94%', display: 'flex', justifyContent: "center" }}>
+                        <div style={{ display: 'flex', gap: "1rem" }}>
+
+                            <img src={cap} alt="" />
+                            <img src={page} alt="" />
+                            <img src={Statis} alt="" />
+                            <img src={moneybag} alt="" />
+                            <img src={emoji} alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className='grey-container' style={{ minHeight: "70px", paddingTop: "15px" }}>
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <div className='innerwhitediv' style={{ minHeight: "110px", width: '94%', marginBottom: "10px" }}>
+                            <div >
+
+                                <div style={{ display: "flex", justifyContent: "center" }}>
+                                    <img src={inviteimg} alt="" style={{ height: "50px", width: "50px", marginTop: "10px" }} />
+                                </div>
+                                <div style={{ padding: "10px", textAlign: 'left', fontSize: '0.7rem' }}>
+                                    <p>
+                                        You will become eligible for volunteer role in communities and your friend will get 50 rs.
+                                        <br /> you will get gold badge.
+                                        <br /> you will become eligible for other premium features.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div><div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <div className='innerwhitediv' style={{ minHeight: "110px", width: '94%', marginBottom: "10px" }}>
+                            <div >
+
+                                <div style={{ display: "flex", justifyContent: "center" }}>
+                                    <img src={inviteimg} alt="" style={{ height: "50px", width: "50px", marginTop: "10px" }} />
+                                </div>
+                                <div style={{ padding: "10px", textAlign: 'left', fontSize: '0.7rem' }}>
+                                    <p>
+                                        You will become eligible for volunteer role in communities and your friend will get 50 rs.
+                                        <br /> you will get gold badge.
+                                        <br /> you will become eligible for other premium features.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div><div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <div className='innerwhitediv' style={{ minHeight: "110px", width: '94%', marginBottom: "10px" }}>
+                            <div >
+
+                                <div style={{ display: "flex", justifyContent: "center" }}>
+                                    <img src={inviteimg} alt="" style={{ height: "50px", width: "50px", marginTop: "10px" }} />
+                                </div>
+                                <div style={{ padding: "10px", textAlign: 'left', fontSize: '0.7rem' }}>
+                                    <p>
+                                        You will become eligible for volunteer role in communities and your friend will get 50 rs.
+                                        <br /> you will get gold badge.
+                                        <br /> you will become eligible for other premium features.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div><div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <div className='innerwhitediv' style={{ minHeight: "110px", width: '94%', marginBottom: "10px" }}>
+                            <div >
+
+                                <div style={{ display: "flex", justifyContent: "center" }}>
+                                    <img src={inviteimg} alt="" style={{ height: "50px", width: "50px", marginTop: "10px" }} />
+                                </div>
+                                <div style={{ padding: "10px", textAlign: 'left', fontSize: '0.7rem' }}>
+                                    <p>
+                                        You will become eligible for volunteer role in communities and your friend will get 50 rs.
+                                        <br /> you will get gold badge.
+                                        <br /> you will become eligible for other premium features.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <div className='innerwhitediv' style={{ minHeight: "110px", width: '94%', marginBottom: "10px" }}>
+                        </div>
+                    </div>
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <div className='innerwhitediv' style={{ minHeight: "110px", width: '94%', marginBottom: "10px" }}>
+                        </div>
+                    </div>
+                </div>
+                <div className='' style={{ minHeight: "70px" }}>
+                </div>
+
+            </div>
+        </main>
+    )
+}
